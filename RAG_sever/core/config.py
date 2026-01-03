bed575fa2d850f32682167c8cdb4b713ba27a4d2
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-01-03 15:38:39 +0700

    Done FAQ threshold

--- RAG_sever/core/config.py
+++ RAG_sever/core/config.py
@@ -1,5 +1,5 @@
 # core/config.py
 
-FAQ_PATH = "Data/FAQ_Stock.txt"
-FAQ_THRESHOLD = 0.65
+FAQ_PATH = r"D:\Subject\RAG_LLM\RAG_stock\RAG-StockQA\RAG_sever\Data\FAQ_Stock.txt"
+FAQ_THRESHOLD = 0.90
 EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
--- RAG_sever/services/llm_service.py
+++ RAG_sever/services/llm_service.py
@@ -1,10 +1,8 @@
 # services/llm_service.py
 
 def ask_llm(user_question: str) -> dict:
-    """
-    Sau này gắn OpenAI / LLaMA / vLLM
-    """
+
     return {
-        "answer": "Câu hỏi này hiện đang được chuyển cho LLM xử lý.",
+        "answer": "Câu hỏi này sẽ được chuyển cho LLM xử lý.",
         "source": "LLM"
     }
